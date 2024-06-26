68da31792ea5dba15fb4a70a0865979e05c2a636
jakxyzab <161974412+jakxyzab@users.noreply.github.com>
2024-06-26 15:01:56 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,11 +23,7 @@ const Sidebar = ({children}) => {
     const[isOpen ,setIsOpen] = useState(false);
     const toggle = () => setIsOpen (!isOpen);
     const menuItem=[
-        // {
-        //     path:"/",
-        //     name:"bars",
-        //     icon:<FaBars/>
-        // },
+        
         {
             path:"/",
             name:"Home",
@@ -61,25 +57,32 @@ const Sidebar = ({children}) => {
     ]
     return (
         <div className="container">
-           <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
+           <div style={{width: isOpen ? "150px" : "50px"}} className="sidebar">
                <div className="top_section">
+                    
 
-                    <img src=".\images\logo.jpg" alt="logo" />
-                    <h1 style={{display: isOpen ? "block" : "none"}} className="logo">
-            
+                        {/* <img src= './images/logo2.png' alt="logo" /> */}
+                        <h1 style={{display: isOpen ? "block" : "none"}} className="logo">
+                        <div className="logowd">
+                            <img src= './images/logo2.png' alt="logo" />
+                        </div>
                     </h1>
-                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
-                        <FaBars onClick={toggle}/>
-                   </div>
+                        <div style={{marginLeft: isOpen ? "10px" : "0px"}} className="bars">
+                            <FaBars onClick={toggle}/>
+                        </div>
+                    
                </div>
+               <div className="mid_section">
+
                {
                    menuItem.map((item, index)=>(
                        <NavLink to={item.path} key={index} className="link" activeclassName="active">
                            <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                           <div style={{display: isOpen ? "block" : "none"} } className="link_text">{item.name}</div>
                        </NavLink>
                    ))
                }
+               </div>
            </div>
            <main>{children}</main>
         </div>
